89604bec460f1a4f0bac5dcb9d8885ed2e37dca0
Pablo B. <39810564+Private04@users.noreply.github.com>
2025-01-04 23:52:23 +0100

    Cabecera de lista_circular

--- TAD_Lista/lista_circular.h
+++ TAD_Lista/lista_circular.h
@@ -1,11 +1,25 @@
 #ifndef LISTA_CIRCULAR_H
 #define LISTA_CIRCULAR_H
 template <typename T>
-class lista_circular{
+class listaCir{
     public:
-        
+        listaCir();
+        size_t tama() const;
+        bool vacia() const;
+        void insertar(const T& x, posicion p);
+        void eliminar(posicion p);
+        const T& elemento(posicion p) const;
+        posicion inipos() const;
+        posicion siguiente(posicion p) const;
+        posicion anterior(posicion p) const;
         const posicion POS_NULA=nullptr;
     private:
-
+        struct nodo{
+            T elemento;
+            nodo *sig;
+            nodo(nodo * p=nullptr) : sig(p) {} 
+        }
+        size_t n_elementos;
+        nodo * L;
 };
 #endif
