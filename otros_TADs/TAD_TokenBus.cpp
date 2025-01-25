b02de0863c15447c53bf3e32d34fee6fe1f329dc
Pablo B. <39810564+Private04@users.noreply.github.com>
2025-01-25 18:14:17 +0100

    Ahora si terminado

--- otros_TADs/TAD_TokenBus.cpp
+++ otros_TADs/TAD_TokenBus.cpp
@@ -6,7 +6,43 @@ TokenBus::Identificador::Identificador(const char *c) : id()
     strcpy(id,c);
 }
 
-TokenBus::TokenBus(const Identificador& id)
+TokenBus::TokenBus(const Identificador& id) : L()
 {
-    
+    L.insertar(id,L.fin());
+    testigo=L.primera();
+}
+
+const TokenBus::Identificador& TokenBus::token() const
+{
+    return L.elemento(testigo);
+}
+
+void TokenBus::insertar_ordenador(const Identificador& id)
+{
+    assert(L.tama()<MaxOrdenadores);
+    L.insertar(id,L.fin());
+}
+
+void TokenBus::eliminar_ordenador()
+{
+    assert(L.tama()>1);
+    L.eliminar(L.primera());
+}
+
+void TokenBus::pasar_token()
+{
+    testigo=L.siguiente(testigo);
+    if(testigo==L.fin())
+    {
+        testigo=L.primera();
+    }
+}
+
+void TokenBus::enviar_trama(const Identificador& b,const Trama t)
+{
+    while(b.id!=L.elemento(testigo).id)
+    {
+        pasar_token();
+    }
+    /*Operar la trama*/
 }
